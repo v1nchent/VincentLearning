07995b572af5c0f15e7a1c88822ccb6b6b5c5947
v1nchent <50679385+v1nchent@users.noreply.github.com>
2019-05-15 22:26:09 +0200

    Add files via upload

---
+++ DatabaseManagementTool/FirstTry/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+
+namespace FirstTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var DatabaseInformation = "Host=localhost;Username=Vincent;Password=VerySecurePassword;Database=GettingStarted";
+
+            using (var connection = new NpgsqlConnection(DatabaseInformation))
+            {
+                connection.Open();
+
+                using (var command = new NpgsqlCommand("SELECT title, price FROM products WHERE price < 25", connection))
+                {
+                    Console.WriteLine("All the books in the database which cost less that 25 Euro are:");
+                    using (var reader = command.ExecuteReader())
+                        while (reader.Read())
+                            Console.WriteLine(System.Convert.ToString(reader.GetValue(0)) + " which costs "+ System.Convert.ToString(reader.GetValue(1)) + "Euro's");
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
